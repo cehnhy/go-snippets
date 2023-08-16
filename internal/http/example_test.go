ef7e9762364422b15d54d1c555f530998d653ce3
cehnhy <cehnhy@outlook.com>
2023-08-16 16:44:06 +0800

    add http

---
+++ internal/http/example_test.go
@@ -0,0 +1,35 @@
+package http_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	internalhttp "github.com/cehnhy/go-snippets/internal/http"
+)
+
+func ExampleNewTransport() {
+	hook := func(req *http.Request) {
+		ctx := req.Context()
+		fmt.Println(ctx.Value("key"))
+	}
+	transport := internalhttp.NewTransport(nil, hook)
+	client := http.Client{
+		Transport: transport,
+	}
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "key", "value")
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.google.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	// Output:
+	// value
+}
---
+++ internal/http/http.go
@@ -0,0 +1,28 @@
+package http
+
+import "net/http"
+
+type transport struct {
+	transport http.RoundTripper
+	hook      func(*http.Request)
+}
+
+func NewTransport(rt http.RoundTripper, hook func(*http.Request)) *transport {
+	if rt == nil {
+		rt = http.DefaultTransport
+	}
+
+	if hook == nil {
+		hook = func(*http.Request) {}
+	}
+
+	return &transport{
+		transport: rt,
+		hook:      hook,
+	}
+}
+
+func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	t.hook(req)
+	return t.transport.RoundTrip(req)
+}
